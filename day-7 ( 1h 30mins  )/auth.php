463c13e62570d25d37454f402c4ea7896b45eb4c
chourabi-dev <chourabitahers62@gmail.com>
2022-06-06 19:10:30 +0100

    latest

---
+++ day-7 ( 1h 30mins  )/auth.php
@@ -0,0 +1,50 @@
+<?php
+
+    session_start();
+
+
+    require_once('connection_db.php');
+
+
+
+    if (isset( $_POST['email'] ) and isset( $_POST['password'] )  ) {
+
+
+        $email = $_POST['email'];
+        $password = sha1($_POST['password']);
+
+
+
+        // mysqli_query
+        $req = $con->prepare('SELECT * FROM `users` WHERE `email` = ? AND `password` = ?');
+        $req->execute(array($email,$password));
+
+
+
+        $res = $req->rowCount();
+
+   
+        
+        if ( $res == 0 ) {
+            header('Location:login.php?message=wrong username or password&error=ok');
+            
+        }else{
+            $data = $req->fetch(); 
+            $_SESSION['id'] = $data['idusers'];
+            $_SESSION['role'] = $data['role'];
+            
+            if ( $data['role'] == 'ROLE_USER' ) {
+                header('Location:index.php');
+            }else if( $data['role'] == 'ROLE_TECH' ){
+                header('Location:interventions.php');
+            }else if( $data['role'] == 'ROLE_ADMIN' ){
+                header('Location:interventions.php');
+            }
+        }
+  
+
+
+        
+    }
+
+?>
